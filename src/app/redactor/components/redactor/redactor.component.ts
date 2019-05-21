269f66f773f30210d4fa82559a2fb232026a063a
s.nedobuga <s.nedobuga@telda.ru>
2019-05-21 18:48:15 +0300

    - key listeners; - fix;

--- src/app/redactor/components/redactor/redactor.component.ts
+++ src/app/redactor/components/redactor/redactor.component.ts
@@ -1,18 +1,21 @@
-import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, HostListener, ViewChild} from '@angular/core';
 import * as joint from 'jointjs';
+import {fromEvent} from 'rxjs';
+import {debounceTime} from 'rxjs/operators';
 
 @Component({
   selector: 'app-redactor',
   templateUrl: './redactor.component.html',
   styleUrls: ['./redactor.component.scss']
 })
-export class RedactorComponent implements OnInit, AfterViewInit {
+export class RedactorComponent implements AfterViewInit {
 
   constructor() {
   }
 
   graph: any;
   paper: any;
+  @ViewChild('paperView') paperView;
 
   blocks: any[] = [
     {
@@ -39,8 +42,6 @@ export class RedactorComponent implements OnInit, AfterViewInit {
       cell: null
     }
   ];
-
-  @ViewChild('paperView') paperView;
 
   static attrByClassId(block) {
     if (block.classId === 1) {
@@ -78,10 +79,8 @@ export class RedactorComponent implements OnInit, AfterViewInit {
     }
   }
 
-  ngOnInit() {
-  }
-
   ngAfterViewInit(): void {
+    // создаем граф и лист для него
     this.graph = new joint.dia.Graph;
     this.paper = new joint.dia.Paper({
       el: this.paperView.nativeElement,
@@ -98,69 +97,80 @@ export class RedactorComponent implements OnInit, AfterViewInit {
       },
       // embeddingMode: true
     });
-
+    // прогоняем массивы
     this.processAllBlocks();
     this.processAllLinks();
 
+    // при изменении позиции чего-либо
     this.graph.on('change:position', (cell) => {
-      // при изменении позиции чего-либо находим умные пути для дорог
       // if (obstacles.indexOf(cell) > -1) this.paper.findViewByModel(link).update();
+      // находим умные пути для всех дорог
       this.links.forEach(l => {
         this.paper.findViewByModel(l.cell).update();
       });
+    });
+    fromEvent(this.paperView.nativeElement, 'keydown').pipe(debounceTime(100)).subscribe(res => {
+      console.log('ohMyGOd', res);
     });
   }
 
   processBlock(block) {
     block.cell = new joint.shapes.standard.Rectangle();
     block.cell.position(block.x, block.y);
-    block.cell.resize(80, 30);
+    block.cell.resize(120, 40);
     RedactorComponent.attrByClassId(block);
     this.graph.addCell(block.cell);
     console.log(block.cell);
   }
 
   processAllBlocks() {
-    this.blocks.forEach(b => {
-      this.processBlock(b);
-    });
+    if (this.blocks && this.blocks.length) {
+      this.blocks.forEach(b => {
+        this.processBlock(b);
+      });
+    }
+  }
+
+  processLink(link) {
   }
 
   processAllLinks() {
-    this.links.forEach(l => {
-      const sourceId = this.blocks.find(b => b.id === l.from).cell.id;
-      const targetId = this.blocks.find(b => b.id === l.to).cell.id;
-      l.cell = new joint.shapes.standard.Link({
-        source: {id: sourceId},
-        target: {id: targetId},
-        router: {name: 'manhattan'},
-        connector: {name: 'rounded'},
-        attrs: {
-          line: {
-            stroke: '#444444',
-            strokeWidth: 2
+    if (this.links && this.links.length) {
+      this.links.forEach(l => {
+        const sourceId = this.blocks.find(b => b.id === l.from).cell.id;
+        const targetId = this.blocks.find(b => b.id === l.to).cell.id;
+        l.cell = new joint.shapes.standard.Link({
+          source: {id: sourceId},
+          target: {id: targetId},
+          router: {name: 'manhattan'},
+          connector: {name: 'rounded'},
+          attrs: {
+            line: {
+              stroke: '#444444',
+              strokeWidth: 2
+            }
           }
-        }
-      });
-      l.cell.attr('line/targetMarker', {
-        type: 'circle', // SVG Circle
-        fill: '#666',
-        stroke: '#333',
-        r: 2, // radius of the circle
-        cx: 1 // move the centre of the circle 5 pixels from the end of the path
-      });
-      l.cell.attr('line/sourceMarker', {
-        type: 'circle', // SVG Circle
-        fill: '#666',
-        stroke: '#333',
-        r: 2, // radius of the circle
-        cx: 1 // move the centre of the circle 5 pixels from the end of the path
-      });
-      this.graph.addCell(l.cell);
-      l.cell.toBack();
+        });
+        l.cell.attr('line/targetMarker', {
+          type: 'circle', // SVG Circle
+          fill: '#666',
+          stroke: '#333',
+          r: 2, // radius of the circle
+          cx: 1 // move the centre of the circle 5 pixels from the end of the path
+        });
+        l.cell.attr('line/sourceMarker', {
+          type: 'circle', // SVG Circle
+          fill: '#666',
+          stroke: '#333',
+          r: 2, // radius of the circle
+          cx: 1 // move the centre of the circle 5 pixels from the end of the path
+        });
+        this.graph.addCell(l.cell);
+        // l.cell.toBack();
 
-      console.log(l.cell);
-    });
+        console.log(l.cell);
+      });
+    }
   }
 
   addBlock(classId) {
@@ -172,5 +182,33 @@ export class RedactorComponent implements OnInit, AfterViewInit {
     };
     this.blocks.push(block);
     this.processBlock(block);
+  }
+
+  @HostListener('window:keyup', ['$event'])
+  keyEventUp(event: KeyboardEvent) {
+    if (event.key === 'Control') {
+      this.ctrlDownedYet = false;
+      this.blocks.forEach(b => {
+          b.cell.attr({rect: {cursor: 'move'}});
+        }
+      );
+    }
+  }
+
+  ctrlDownedYet = false;
+
+  @HostListener('window:keydown', ['$event'])
+  ctrlDown(event: KeyboardEvent) {
+    if (event.key === 'Control') {
+      if (!this.ctrlDownedYet) {
+        this.ctrlDownedYet = true;
+        this.blocks.forEach(b => {
+            b.cell.attr({rect: {cursor: 'pointer'}});
+            b.cell.stopTransitions();
+          }
+        );
+      }
+    }
+
   }
 }
